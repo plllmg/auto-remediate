3cfdc93893087950bbfb703a84b35734430c3b6c
Paul MG <pmangeof@gmail.com>
2019-10-07 14:34:05 -0500

    support for legacy accts EC2 SG access revoker

--- utils/security_group_access_revoker.js
+++ utils/security_group_access_revoker.js
@@ -8,18 +8,34 @@ function revokeSecurityGroupAccess (protocol, port, resource, region) {
         FromPort: port,
         ToPort: port,
         IpProtocol: protocol,
-        IpRanges: [{ CidrIp: '0.0.0.0/0' }]
-      },
-      {
-        FromPort: port,
-        ToPort: port,
-        IpProtocol: protocol,
         Ipv6Ranges: [{ CidrIpv6: '::/0' }]
       }
     ]
   }
 
   let ec2 = new AWS.EC2({ region: region })
+
+  ec2.revokeSecurityGroupIngress(params, function (err, result) {
+    if (err) {
+      throw err
+    }
+
+    console.log('Result', result)
+    return result
+  })
+
+
+  params = {
+    GroupId: resource,
+    IpPermissions: [
+      {
+        FromPort: port,
+        ToPort: port,
+        IpProtocol: protocol,
+        IpRanges: [{ CidrIp: '0.0.0.0/0' }]
+      }
+    ]
+  }
 
   ec2.revokeSecurityGroupIngress(params, function (err, result) {
     if (err) {
